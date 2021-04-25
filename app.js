1fb355ed9b36a5b53fe85fbe2f9931216cfb70fb
lotiko <l.despres@netc.fr>
2021-04-25 16:07:29 +0200

    iteration 4 ans start 5

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const express = require("express");
 const hbs = require("hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
@@ -36,11 +37,72 @@ app.get("/artist-search", function (req, res) {
   spotifyApi
     .searchArtists(req.query.search)
     .then((data) => {
-      console.log("The received data from the API: ", data.body.artists.items[0].images[0]);
+      console.log("The received data from the API!");
+      let dataView = [];
+      data.body.artists.items.map((val) => {
+        if (val.images[0]) {
+          dataView.push(createDataCard(val, "artists"));
+        }
+      }, []);
+      console.log(dataView);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-      res.render("artist-search-results", { data: data }); // on va rendre la view ds laquelle on injecte la data
+      res.render("artist-search-results", { data: dataView }); // on va rendre la view ds laquelle on injecte la data
     })
     .catch((err) => console.log("The error while searching artists occurred: ", err));
 });
+app.get("/albums/:id", async (req, res, next) => {
+  try {
+    let dataSpotify = await spotifyApi.getArtistAlbums(req.params.id);
+    let dataView = dataSpotify.body.items.map((element) => createDataCard(element, "albums"));
+    console.log(dataView);
+    res.render("albums-search-results", { data: dataView });
+  } catch (error) {
+    console.log("Oops error => ", error);
+  }
+});
+app.get("/tracks/:id", async (req, res, next) => {
+  try {
+    let dataSpotify = await spotifyApi.getAlbumTracks(req.params.id);
+    //let dataView = dataSpotify.body.items.map((element) => createDataCard(element, "tracks"));
+    let test = await spotifyApi.getAudioFeaturesForTrack(dataSpotify.body.items[0].id);
+    // let test = await spotifyApi.getAudioAnalysisForTrack(dataSpotify.body.items[0].id);
+    console.log(dataSpotify.body.items);
+    res.render("tracks-search-results" /*, { data: dataView }*/);
+  } catch (error) {
+    console.log("Oops error => ", error);
+  }
+});
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
+
+/**
+ * Conserve aspect ratio of the original region. Useful when shrinking/enlarging
+ * images to fit into a certain area.
+ *
+ * @param {Number} srcWidth width of source image
+ * @param {Number} srcHeight height of source image
+ * @param {Number} maxWidth maximum available width
+ * @param {Number} maxHeight maximum available height
+ * @return {Object} { width, height }
+ */
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+  var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+  return { width: srcWidth * ratio, height: srcHeight * ratio };
+}
+
+function createDataCard(srcData, type) {
+  let newdata = {};
+  if (srcData.images[0]) {
+    newdata = {
+      name: srcData.name,
+      image: {
+        url: srcData.images[0].url,
+        size: calculateAspectRatioFit(srcData.images[0].width, srcData.images[0].height, 150, 150),
+      },
+      id: srcData.id,
+    };
+    return { ...newdata, type: type, subType: type === "artists" ? "albums" : "tracks" };
+  }
+  return;
+}
---
+++ model/spotifyclas.js
@@ -0,0 +1,5 @@
+// class SpotifyClass {
+//     constructor() {
+//         this.baseUrl =
+//     }
+// }
